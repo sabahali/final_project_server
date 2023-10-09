602daea4950dc921184f98f9462ca2b236d7f137
sabahali <sabahakick21@gmail.com>
2023-10-09 22:26:24 +0530

    added Access Control

--- middlewares/credentials.js
+++ middlewares/credentials.js
@@ -1,6 +1,7 @@
 const credentials = (req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    res.header('Access-Control-Allow-Origin', 'https://e-commerce-client-rlwo.onrender.com');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('ngrok-skip-browser-warning',true);
     res.header('Access-Control-Allow-Credentials', true);

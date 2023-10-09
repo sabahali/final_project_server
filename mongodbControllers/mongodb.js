43844cabf20fc8dd9281c643e23c7d1039b79e7c
sabahali <sabahakick21@gmail.com>
2023-10-09 20:38:43 +0530

    addedmognodbatlasstring

--- mongodbControllers/mongodb.js
+++ mongodbControllers/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 //mongodb://127.0.0.1:27017/jwtdatabase
 async function connect(callback) {
     try {
-        const response = await mongoose.connect('mongodb://127.0.0.1:27017/jwtdatabase', {
+        const response = await mongoose.connect("mongodb+srv://sabahali:678601@cluster0.d2hxxpn.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
